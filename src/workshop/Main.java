84bf91c7d3320404972da59c61c7f75143f339cb
PC-DOUWE\Douwe <douwe.schepers@student.hu.nl>
2018-02-23 10:46:22 +0100

    commit 3

--- src/workshop/Main.java
+++ src/workshop/Main.java
@@ -2,6 +2,6 @@ package workshop;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("first commit step 2 -//S-");
+		System.out.println("I don't feel like doing this workshop but I have to because it is 5 percent of my grade");
 	}
 }
